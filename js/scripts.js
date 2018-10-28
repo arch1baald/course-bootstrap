96697e5b5c17776d93526384de88809bba446540
Dmitry Savostyanov <ds@gosu.ai>
2018-10-28 14:14:15 +0200

    on change build: https://www.coursera.org/learn/bootstrap-4/supplement/nIb73/exercise-instructions-npm-scripts-part-1

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    $('#mycarousel').carousel({ interval: 2000 });
+    $('#carouselButton').click(function () {
+        if ($(this).children('span').hasClass('fa-pause')) {
+            $('#mycarousel').carousel('pause');
+            $(this).children('span').removeClass('fa-pause');
+            $(this).children('span').addClass('fa-play');
+        } else if ($('#carouselButton').children('span').hasClass('fa-play')) {
+            $('#mycarousel').carousel('cycle');
+            $(this).children('span').removeClass('fa-play');
+            $(this).children('span').addClass('fa-pause');
+        }
+    });
+
+    <!-- To debug: console.log("") -->
+    <!-- Assignment 4, Tasks 1, 2, 3 -->
+    let idLoginModal = '#loginModal';
+    $('#navBarLoginButton').click(function () {
+        $(idLoginModal).modal('show');
+    });
+    $('#loginCloseCross').click(function () {
+        $(idLoginModal).modal('hide');
+    });
+    $('#loginCancelButton').click(function () {
+        $(idLoginModal).modal('hide');
+    });
+
+    let idReserveModal = '#reserveModal';
+    $('#reserveTableButton').click(function () {
+        $(idReserveModal).modal('show')
+    });
+    $('#reserveCloseCross').click(function () {
+        $(idReserveModal).modal('hide')
+    });
+    $('#reserveCancelButton').click(function () {
+        $(idReserveModal).modal('hide')
+    });
+});
